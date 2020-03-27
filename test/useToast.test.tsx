777005c838e981dbc0d9690589a58ecdbcb5e1c3
Agney <agney.menon@bigbinary.com>
2020-03-27 22:14:31 +0530

    Delete modified tests

--- test/useToast.test.tsx
+++ test/useToast.test.tsx
@@ -1,8 +1,11 @@
 import * as React from 'react';
-import { ToastProvider } from '..';
-
 import { render } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
+import { mockIonicReact } from '@ionic/react-test-utils';
+
+import { ToastProvider } from '..';
+
+mockIonicReact();
 
 describe('it', () => {
   it('renders children passed to it', () => {
